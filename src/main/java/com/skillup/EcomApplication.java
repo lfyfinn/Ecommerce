2d969a04c8431be60c563e60fc55728214c5f5b4
lfyfinn <lfyyoung55@gmail.com>
2023-10-14 22:21:46 -0400

    create basic class

---
+++ src/main/java/com/skillup/EcomApplication.java
@@ -0,0 +1,13 @@
+package com.skillup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApplication {
+
+    public static void main (String [] args) {
+        SpringApplication.run(EcomApplication.class, args);
+        System.out.println("I'm Feiyang Long");
+    }
+}
